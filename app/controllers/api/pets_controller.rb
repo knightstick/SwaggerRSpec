fd599b04fb125ae09af5b9ae4feedaa699e2ee26
Chris Jewell <chrisjohnjewell@gmail.com>
2016-04-07 14:45:58 +1000

    Add index route to PetsController

--- app/controllers/api/pets_controller.rb
+++ app/controllers/api/pets_controller.rb
@@ -1,2 +1,7 @@
 class Api::PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+
+    render json: @pets, each_serializer: PetSerializer
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  namespace :api, defaults: { format: :json } do
+    get '/', to: 'pets#index'
+  end
 end
